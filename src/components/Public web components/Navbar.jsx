15b6cee79a7ae87fe00702eace3690876b1ddf26
vijayakumar1069 <vijay.r20799@gmail.com>
2024-11-12 15:39:31 +0530

    Your commit message here

---
+++ src/components/Public web components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { navbarvalues } from "@/utils/constvalues";
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#4F9F76] w-full h-20 flex items-center ">
+        <div className="w-[1400px]  flex mx-auto items-center justify-between">
+
+      <div className="flex space-x-2 items-center">
+        <Image src="/logo.png" alt="logo" width={40} height={24} />
+        <h1 className="text-xl    text-white">Med Games</h1>
+      </div>
+      <div className="">
+        <ul>
+          {navbarvalues.map((item) => (
+            <li className="inline-block mx-2 text-white" key={item.id}>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="">
+        <Button className="bg-transparent text-white px-8 py-2 rounded-md hover:bg-transparent hover:text-white border">
+          Get Started
+        </Button>
+      </div>
+        </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/utils/constvalues.js
@@ -0,0 +1,39 @@
+export const navbarvalues=[
+    {
+        id:1,
+         name:"Home",
+  
+         link:"/"
+
+    },
+    {
+        id:2,
+         name:"About",
+         link:"/about"
+    },
+    {
+        id:3,
+         name:"Contact",
+         link:"/contact"
+    },
+    {
+        id:4,
+        name:"Testimonials",
+        link:"/testimonials"
+    },
+    {
+        id:5,
+        name:"Services",
+        link:"/services"
+    },
+    {
+        id:6,
+        name:"Blog",
+        link:"/blog"
+    },
+    {
+        id:7,
+        name:"Upcoming Events",
+        link:"/upcomin_gevents"
+    }
+]
