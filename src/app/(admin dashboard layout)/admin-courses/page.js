6b1caa33b02a7929de7dec5122cc071da119f173
vijayakumar1069 <vijay.r20799@gmail.com>
2024-12-04 16:36:11 +0530

    test repo added

--- src/app/(admin dashboard layout)/admin-courses/page.js
+++ src/app/(admin dashboard layout)/admin-courses/page.js
@@ -1,7 +1,7 @@
 export default function Admin_Courses_page() {
     return (
         <div>
-            <h1>Admin courses page </h1>
+            <h1>Admin courses page  test repo </h1>
         </div>
     );
 }
