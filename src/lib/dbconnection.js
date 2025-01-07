f8a0689329914d5a7aeab303b09e6909f23fa467
vijayakumar1069 <vijay.r20799@gmail.com>
2025-01-07 19:17:24 +0530

    console logs added in dn connection

--- src/lib/dbconnection.js
+++ src/lib/dbconnection.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI)
-
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log("db connected successfully");
     return connection; // Return the connection for debugging or further usage
   } catch (err) {
-
+    console.log("db connection error");
     throw new Error("Failed to connect to the database");
   }
 };
