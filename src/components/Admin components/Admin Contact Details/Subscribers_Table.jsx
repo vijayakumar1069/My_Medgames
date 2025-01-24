b60e00e1f1752ba5c81d5fa13adfb7041aeb0418
vijayakumar1069 <vijay.r20799@gmail.com>
2025-01-24 15:58:33 +0530

    subscriber table size increased

--- src/components/Admin components/Admin Contact Details/Subscribers_Table.jsx
+++ src/components/Admin components/Admin Contact Details/Subscribers_Table.jsx
@@ -19,7 +19,7 @@ const Subscribers_Table = async () => {
     }
 
     return (
-      <div className="max-w-sm text-center mx-auto">
+      <div className="max-w-xl text-center mx-auto">
         <div className="overflow-x-auto">
           <Table>
             <TableHeader>
